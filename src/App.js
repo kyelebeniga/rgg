fa6c9e8098424e39fee627a4b1330fd8f7c74a7f
kyelebeniga <61011686+kyelebeniga@users.noreply.github.com>
2023-01-26 17:37:13 +0800

    Sidebar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
+import './Components/css/styles.css'
+import './Components/css/index.css'
+import './Components/css/sidebar.css'
 import Navbar from './Components/Navbar';
 import Home from './Components/Home';
 import Contacts from './Components/Contacts';
 import Notifications from './Components/Notifications';
+import Sidebar from './Components/Sidebar';
 
 function App() {
   let component 
@@ -23,6 +27,7 @@ function App() {
   }
   return (
     <>
+      <Sidebar />
       <Navbar />
       {component}
     </>
