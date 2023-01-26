8c7e2b528b7ab74cbe490a1c05310e79bac29191
kyelebeniga <61011686+kyelebeniga@users.noreply.github.com>
2023-01-27 04:17:11 +0800

    Github hosting

--- src/Components/Sidebar.js
+++ src/Components/Sidebar.js
@@ -1,8 +1,12 @@
 import React from 'react'
-
+import {FaListAlt} from 'react-icons/fa'
 function Sidebar() {
   return (
     <div className='sidebar'>
+      <div className='item'>
+        <i><FaListAlt /></i>
+        <span>League of Legends</span>
+      </div>
     </div>
   )
 }
