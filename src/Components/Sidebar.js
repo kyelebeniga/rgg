48bccab38bc9ccee50f020c169a1432a66177598
kyelebeniga <61011686+kyelebeniga@users.noreply.github.com>
2023-01-29 22:35:18 +0800

    More sidebar features

--- src/Components/Sidebar.js
+++ src/Components/Sidebar.js
@@ -1,17 +1,26 @@
 import React from 'react'
-import {FaListAlt} from 'react-icons/fa'
+import {
+  FaListAlt,
+  FaSkull,
+  FaSearch,
+  FaRegDotCircle,
+  FaTrophy,
+  FaPen,
+  FaPushed
+} from 'react-icons/fa';
+import LeagueIcon from '../Assets/licon.png'
 function Sidebar() {
   return (
     <div className='sidebar'>
       <ul>
-        <li><a href='#'><i><FaListAlt /></i> League of Legends</a></li>
+        <li><a href='#'><i><img src={LeagueIcon} /></i> League of Legends</a></li>
         <li><a href='#'><i><FaListAlt /></i> Tier List</a></li>
-        <li><a href='#'><i><FaListAlt /></i> Champions</a></li>
-        <li><a href='#'><i><FaListAlt /></i> Multisearch</a></li>
-        <li><a href='#'><i><FaListAlt /></i> Live Games</a></li>
-        <li><a href='#'><i><FaListAlt /></i> Leaderboards</a></li>
-        <li><a href='#'><i><FaListAlt /></i> Items</a></li>
-        <li><a href='#'><i><FaListAlt /></i> Probuild Stats</a></li>
+        <li><a href='#'><i><FaSkull /></i> Champions</a></li>
+        <li><a href='#'><i><FaSearch /></i> Multisearch</a></li>
+        <li><a href='#'><i><FaRegDotCircle /></i> Live Games</a></li>
+        <li><a href='#'><i><FaTrophy /></i> Leaderboards</a></li>
+        <li><a href='#'><i><FaPen /></i> Items</a></li>
+        <li><a href='#'><i><FaPushed /></i> Probuild Stats</a></li>
       </ul>
     </div>
   )
